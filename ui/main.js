f4da69d5900b7eb65c9cde075d80dfe03d146609
muhammedmsr <muhammedmsr@gmail.com>
2016-10-15 09:42:47 -0700

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -25,4 +25,6 @@ button.onclick=function()
     for(var i=0;i<names.length;i++)
     {list+='<li>'+names[i]+'</li>';
     }
+    var ul=document.getElementById('namelist');
+    ul.innerHTML=list;
 };
