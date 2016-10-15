90fa0f7e1a1f96790aa33d5f703fe83be0874301
muhammedmsr <muhammedmsr@gmail.com>
2016-10-15 09:17:57 -0700

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,1 +1,14 @@
-console.log('Loaded!');
+var button=document.getElementById('counter');
+button.onclick=function()
+{  
+ var request=new XMLHttpRequest();
+ request.onreadystatechange=function()
+  {if(request.readystate===XMLHttpRequest,DONE)
+     if(request.status==200)
+      {
+      }
+  };
+    counter=counter+1;
+    var span =document.getElementById('count');
+    span.innerHTML=counter.toString();
+};
